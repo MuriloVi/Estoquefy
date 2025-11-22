10e613ef39a3e7e817dcc3a7dc675ec4b7a45ece
MuriloVi <murilovif17@gmail.com>
2025-11-22 17:11:32 -0300

    WIP: Dependencies , Riverpod and Supabase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-void main() {
-  runApp(const EstoquefyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  
+  runApp(
+    const ProviderScope(
+      child: EstoquefyApp(),
+    ),
+  );
 }
 
 class EstoquefyApp extends StatelessWidget {
---
+++ lib/repositories/supabase_client.dart
@@ -0,0 +1,16 @@
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+class SupabaseClientConfig {
+  static Future<void> initialize({
+    required String url,
+    required String anonKey,
+  }) async {
+    await Supabase.initialize(
+      url: url,
+      anonKey: anonKey,
+    );
+  }
+
+  static SupabaseClient get client => Supabase.instance.client;
+}
+
